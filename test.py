2627802bc8a8d189e7b02e37b9e7a966695e855a
aki29.yu <aki29.yu@gmail.com>
2025-07-16 20:12:56 +0800

    update

--- test.py
+++
@@ -1,5 +0,0 @@
-from webrtc_audio_processing import AudioProcessingModule as AP
-
-ap = AP(enable_ns=True)          # 只先開 Noise Suppression
-ap.set_stream_format(16000, 1)   # 16 kHz，單聲道
-print("Noise Suppressor initialised ✔")
